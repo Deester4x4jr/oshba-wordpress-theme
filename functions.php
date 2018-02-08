586584161a26ac7830bb416b532767a9ae1531de
Deester4x4jr <josh@thaw.io>
2018-02-08 08:14:24 -0700

    Theme changes committed by Gulp-Git - 2/8/2018, 8:14:24 AM

--- functions.php
+++ functions.php
@@ -94,8 +94,48 @@ function bulma_menu() {
     $menu = get_term(get_nav_menu_locations()['header-menu'], 'nav_menu')->name;
     $menu_items = wp_get_nav_menu_items($menu);
 
+    foreach( $menu_items as $menu_item ) {
+             
+        $link = $menu_item->url;
+        $title = $menu_item->title;
+         
+        if ( !$menu_item->menu_item_parent ) {
+            $parent_id = $menu_item->ID;
+             
+            $menu_list .= '<li class="item">' ."\n";
+            $menu_list .= '<a href="'.$link.'" class="title">'.$title.'</a>' ."\n";
+        }
+
+        if ( $parent_id == $menu_item->menu_item_parent ) {
+
+            if ( !$submenu ) {
+                $submenu = true;
+                $menu_list .= '<ul class="sub-menu">' ."\n";
+            }
+
+            $menu_list .= '<li class="item">' ."\n";
+            $menu_list .= '<a href="'.$link.'" class="title">'.$title.'</a>' ."\n";
+            $menu_list .= '</li>' ."\n";
+                 
+
+            if ( $menu_items[ $count + 1 ]->menu_item_parent != $parent_id && $submenu ){
+                $menu_list .= '</ul>' ."\n";
+                $submenu = false;
+            }
+
+        }
+
+        if ( $menu_items[ $count + 1 ]->menu_item_parent != $parent_id ) { 
+            $menu_list .= '</li>' ."\n";      
+            $submenu = false;
+        }
+
+        $count++;
+    }
+
     // $output = '<nav class="level" role="navigation">'..'</nav>';
     echo '<pre>';
+    echo count($menu_items);
     print_r($menu_items);
     echo '</pre>';
 
