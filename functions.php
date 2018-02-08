62abccd95413cd0a24d6a4307a1bc2e8ce75da63
Deester4x4jr <josh@thaw.io>
2018-02-08 07:38:33 -0700

    Theme changes committed by Gulp-Git - 2/8/2018, 7:38:33 AM

--- functions.php
+++ functions.php
@@ -116,7 +116,7 @@ function html5blank_conditional_scripts()
 // Load HTML5 Blank styles
 function html5blank_styles()
 {
-    wp_register_style('html5blank', get_template_directory_uri() . 'assets/css/main/style.css', array(), '1.0', 'all');
+    wp_register_style('html5blank', get_template_directory_uri() . 'assets/css/main/style.min.css', array(), '1.0', 'all');
     wp_enqueue_style('html5blank'); // Enqueue it!
 }
 
