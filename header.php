90ad9fe5afecffcc8af96f2358aabeba810739da
Deester4x4jr <josh@thaw.io>
2018-02-08 10:35:13 -0700

    Theme changes committed by Gulp-Git - 2/8/2018, 10:35:13 AM

--- header.php
+++ header.php
@@ -36,7 +36,9 @@
 						<div class="container">
   							
   							<div class="navbar-brand">
-  								<a href="/"><img src="https://bulma.io/images/bulma-type.png"></a>
+  								<a class="navbar-item" href="https://bulma.io">
+									<img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+								</a>
   							</div>
 
   							<div id="main-nav" class="navbar-menu">
