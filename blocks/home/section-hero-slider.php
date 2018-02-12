d6d4e06c964e24e38531a648d738f735c4ea1566
Deester4x4jr <josh@thaw.io>
2018-02-12 01:05:23 -0700

    Theme changes committed by Gulp-Git - 2/12/2018, 1:05:23 AM

--- blocks/home/section-hero-slider.php
+++ blocks/home/section-hero-slider.php
@@ -1,6 +1,49 @@
+<?php
+
+  // Get items for slider
+  $posts = get_posts(array(
+    'post_type'         => array('event','post','resource'),
+    'orderby'           => 'date',
+    'order'             => 'DESC',
+    'posts_per_page'    => 6,
+  ));
+
+?>
+
 <!-- Hero Slider Section -->
-<section class="hero">
-  <div class="hero-body">
+<section class="hero is-medium">
+  <div class="container">
+    <div class=".slider-for">
+      <?php
+        
+        foreach ($posts as $post) {
+          
+          setup_postdata();
+
+          echo '<div><a href="'.get_permalink().'"><img src="'.get_the_post_thumbnail_url(get_the_ID(),'full').'" /></a></div>';
+
+          wp_reset_postdata();
+        }
+
+      ?>
+    </div>
+    <div class=".slider-nav">
+      <?php
+        
+        foreach ($posts as $post) {
+          
+          setup_postdata();
+
+          echo '<div><a href="'.get_permalink().'"><img src="'.get_the_post_thumbnail_url(get_the_ID(),'full').'" /></a></div>';
+
+          wp_reset_postdata();
+        }
+
+      ?>
+    </div>
+  </div>
+
+  <!-- <div class="hero-body">
     <div class="container">
       <h1 class="title">
         Hero title
@@ -9,6 +52,25 @@
         Hero subtitle
       </h2>
     </div>
-  </div>
+  </div> -->
 </section>
 <!-- End Hero Slider Section -->
+
+<script type="text/javascript">
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+</script>
