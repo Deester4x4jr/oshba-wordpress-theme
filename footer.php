277aa3c6d63499042a6152e559525492a476071c
Deester4x4jr <josh@thaw.io>
2018-02-12 09:11:32 -0700

    Theme changes committed by Gulp-Git - 2/12/2018, 9:11:32 AM

--- footer.php
+++ footer.php
@@ -1,30 +1,35 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
+<!-- wrapper -->
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-				</p>
-				<!-- /copyright -->
+	<!-- footer -->
+	<footer class="footer">
+	    
+	    <!-- footer container -->
+	    <div class="container">
+	        
+	        <!-- content -->
+	        <div class="content has-text-centered">
+	            <p>
+	                <strong>Bulma</strong> by <a href="https://jgthms.com"> Jeremy Thomas</a>. The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+	            </p>
+	        </div>
+	        <!-- /content -->
+	    
+	    </div>
+	    <!-- /footer container -->
 
-			</footer>
-			<!-- /footer -->
+	</footer>
+	<!-- /footer -->
 
-		</div>
-		<!-- /wrapper -->
+<!-- /wrapper -->
 
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
-		<!-- analytics -->
-		<script>
+<!-- analytics -->
+<!-- <script>
 		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
 		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
 		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
 		ga('send', 'pageview');
-		</script>
-
-	</body>
-</html>
+		</script> -->
