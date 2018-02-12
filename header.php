416e2608c67f4083b0213968bcb351db9612ace1
Deester4x4jr <josh@thaw.io>
2018-02-11 21:10:43 -0700

    Theme changes committed by Gulp-Git - 2/11/2018, 9:10:43 PM

--- header.php
+++ header.php
@@ -26,9 +26,9 @@
 		<meta name="twitter:description" content="Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis." />
 		<meta name="twitter:title" content="Some Cool Title" />
 		<meta name="twitter:site" content="@Deester4x4jr" />
-		<meta name="twitter:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-horizontal.png" />
 		<meta name="twitter:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-vertical.png" />
 		<meta name="twitter:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-square.png" />
+		<meta name="twitter:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-horizontal.png" />
 
 		<?php wp_head(); ?>
 		<script>
