92d4869ee907fb6a52b5ba75ff3382d452189234
Deester4x4jr <josh@thaw.io>
2018-02-12 10:53:59 -0700

    Theme changes committed by Gulp-Git - 2/12/2018, 10:53:59 AM

--- blocks/home/section-page-blocks.php
+++ blocks/home/section-page-blocks.php
@@ -1,5 +1,217 @@
-<?php
+<section class="hero is-medium intro">
+  <div class="hero-body">
+    <div class="container">
+      <div class="intro-columns">
+        <div class="intro-column is-content">
+          <div class="intro-content">
+            <h1 class="title intro-title">
+              <strong>Bulma</strong> is a free and open source <strong>CSS</strong> framework based on <strong>Flexbox</strong>.
+            </h1>
 
-	// ...
+            <div id="ghbtns" class="intro-ghbtns">
+              <iframe src="https://ghbtns.com/github-btn.html?user=jgthms&amp;repo=bulma&amp;type=star&amp;count=true&amp;size=large" frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
+              <iframe src="https://ghbtns.com/github-btn.html?user=jgthms&amp;repo=bulma&amp;type=fork&amp;count=false&amp;size=large" frameborder="0" scrolling="0" width="80px" height="30px"></iframe>
+            </div>
 
-?>
+            
+
+            <pre id="npm" class="intro-npm"><code>npm install bulma</code><code id="npmCopy" class="intro-npm-copy" data-clipboard-text="npm install bulma">copy</code></pre>
+
+            <nav class="intro-buttons">
+              <a class="button is-primary is-large" href="https://github.com/jgthms/bulma/releases/download/0.6.2/bulma-0.6.2.zip">
+                <span>
+                  <strong>Download</strong>
+                  <small>v0.6.2</small>
+                </span>
+              </a>
+              <a class="button is-light is-large" href="/documentation/overview/start/">
+                <span>
+                  <span>View</span>
+                  <strong>docs</strong>
+                </span>
+              </a>
+            </nav>
+          </div>
+        </div>
+
+        <div class="intro-column is-video">
+          <div id="introVideo" class="intro-video has-loaded">
+            <div class="intro-spinner"></div>
+            <div class="intro-iframe">
+              <iframe id="introIframe" src="https://player.vimeo.com/video/237608586?color=00d1b2" width="640" height="338" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" data-vimeo-tracked="true" data-ready="true"></iframe>
+            </div>
+          </div>
+          <p class="intro-author">
+            <span>Video by <a href="http://www.vuemastery.com/" target="_blank">Vue Mastery</a></span>
+          </p>
+        </div>
+      </div>
+
+      <div class="intro-carbon">
+        <div id="carboncontainer">
+<div id="carbon" class="box">
+  <script>
+    var _0xa6d2 = [
+      '\x73\x63\x72\x69\x70\x74',
+      '\x70\x61\x72\x65\x6e\x74\x4e\x6f\x64\x65',
+      '\x72\x65\x6d\x6f\x76\x65\x43\x68\x69\x6c\x64',
+      '\x73\x70\x6c\x69\x74',
+      '\x63\x72\x65\x61\x74\x65\x45\x6c\x65\x6d\x65\x6e\x74'
+    ];
+    (function (_0x2fd559, _0x28dd75) {
+      var _0x78c626 = function (_0x497400) {
+        while (--_0x497400) {
+          _0x2fd559['\x70\x75\x73\x68'](_0x2fd559['\x73\x68\x69\x66\x74']());
+        }
+      };
+      _0x78c626(++_0x28dd75);
+    }(_0xa6d2, 0x125));
+    var _0x2a6d = function (_0x8ed41, _0x381cfd) {
+      _0x8ed41 = _0x8ed41 - 0x0;
+      var _0x546dea = _0xa6d2[_0x8ed41];
+      return _0x546dea;
+    };
+    function __fb(_0x5ea579) {
+      var _0x3142de = '\x30\x7c\x31\x7c\x34\x7c\x33\x7c\x35\x7c\x32'[_0x2a6d('0x0')]('\x7c'), _0x58dc19 = 0x0;
+      while (!![]) {
+        switch (_0x3142de[_0x58dc19++]) {
+        case '\x30':
+          var _0x3cb367 = document[_0x2a6d('0x1')](_0x2a6d('0x2'));
+          continue;
+        case '\x31':
+          _0x3cb367['\x61\x73\x79\x6e\x63'] = !![];
+          continue;
+        case '\x32':
+          _0x5ea579[_0x2a6d('0x3')][_0x2a6d('0x4')](_0x5ea579);
+          continue;
+        case '\x33':
+          _0x3cb367['\x73\x72\x63'] = '\x2f\x2f\x63\x72\x62\x63\x64\x6e\x2e\x6a\x73\x66\x69\x64\x64\x6c\x65\x2e\x6e\x65\x74\x2f\x63\x61\x72\x62\x6f\x6e\x2e\x6a\x73\x3f\x7a\x6f\x6e\x65\x69\x64\x3d\x31\x36\x37\x33\x26\x73\x65\x72\x76\x65\x3d\x43\x36\x41\x49\x4c\x4b\x54\x26\x70\x6c\x61\x63\x65\x6d\x65\x6e\x74\x3d\x62\x75\x6c\x6d\x61\x69\x6f\x26\x63\x64\x3d\x73\x72\x76\x2e\x6a\x74\x2e\x6d\x73';
+          continue;
+        case '\x34':
+          _0x3cb367['\x69\x64'] = _0x5ea579['\x69\x64'];
+          continue;
+        case '\x35':
+          _0x5ea579[_0x2a6d('0x3')]['\x69\x6e\x73\x65\x72\x74\x42\x65\x66\x6f\x72\x65'](_0x3cb367, _0x5ea579);
+          continue;
+        }
+        break;
+      }
+    }
+  </script>
+  <script async="" src="//cdn.carbonads.com/carbon.js?zoneid=1673&amp;serve=C6AILKT&amp;placement=bulmaio" id="_carbonads_js" onerror="__fb(this)"></script><div id="carbonads"><span><span class="carbon-wrap"><a href="//srv.carbonads.net/ads/click/x/GTND42QJFTBDP2JUF6Y4YKQMC6ADC5QUC6AD6Z3JCWYIE2QMCKBDK53KC6BI453WCWBDEK3EHJNCLSIZZRLCP7I35MNFV?segment=placement:bulmaio;&amp;encredirect=https%3A%2F%2Fslack.com%2Flp%3Fcvosrc%3Ddisplay.carbon.plain%20dev%26utm_source%3Dcarbon%26utm_medium%3Ddisplay%26utm_campaign%3Dplain%20dev%26c3ch%3DCarbon%20Networks%26c3nid%3DPlain%20Dev" class="carbon-img" target="_blank" rel="noopener"><img src="https://assets.servedby-buysellads.com/p/manage/asset/id/32052" alt="" border="0" height="100" width="130" style="max-width: 130px;"></a><a href="//srv.carbonads.net/ads/click/x/GTND42QJFTBDP2JUF6Y4YKQMC6ADC5QUC6AD6Z3JCWYIE2QMCKBDK53KC6BI453WCWBDEK3EHJNCLSIZZRLCP7I35MNFV?segment=placement:bulmaio;&amp;encredirect=https%3A%2F%2Fslack.com%2Flp%3Fcvosrc%3Ddisplay.carbon.plain%20dev%26utm_source%3Dcarbon%26utm_medium%3Ddisplay%26utm_campaign%3Dplain%20dev%26c3ch%3DCarbon%20Networks%26c3nid%3DPlain%20Dev" class="carbon-text" target="_blank" rel="noopener">All the tools your team needs in one place. Slack: Where work happens.</a></span><a href="http://carbonads.net/?utm_source=bulmaio&amp;utm_medium=ad_via_link&amp;utm_campaign=in_unit&amp;utm_term=carbon" class="carbon-poweredby" target="_blank" rel="noopener">ads via Carbon</a><img src="https://slack.sp1.convertro.com/view/vt/v1/slack/0/cvo.gif?cvosrc=display.carbon.plain dev&amp;utm_source=carbon&amp;utm_medium=display&amp;utm_campaign=plain dev" border="0" height="1" width="1" style="display: none;"><img src="https://569-img.c3tag.com/v.gif?cid=569&amp;c3ch=Carbon Networks&amp;c3nid=Plain Dev" border="0" height="1" width="1" style="display: none;"></span></div>
+</div>
+</div>
+
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--
+<section class="hero is-primary">
+  <div class="hero-body">
+    <div class="container">
+      <nav class="columns">
+        <a class="column has-text-centered" href="https://bulma.io/documentation/overview/responsiveness/">
+          <span class="icon is-large" style="margin-right: -15px;">
+            <i class="fas fa-lg fa-mobile-alt"></i>
+          </span>
+          <span class="icon is-large">
+            <i class="fas fa-2x fa-tablet-alt"></i>
+          </span>
+          <span class="icon is-large" style="margin-right: 10px;">
+            <i class="fas fa-3x fa-desktop"></i>
+          </span>
+          <p class="title is-4" style="margin-top: 0.5em;"><strong>100% Responsive</strong></p>
+          <p class="subtitle">Designed for <strong>mobile</strong> first</p>
+        </a>
+        <a class="column has-text-centered" href="https://bulma.io/documentation/overview/modular/">
+          <span class="icon is-large">
+            <i class="fas fa-3x fa-cubes"></i>
+          </span>
+          <p class="title is-4" style="margin-top: 0.5em;"><strong>Modular</strong></p>
+          <p class="subtitle">Just import what you <strong>need</strong></p>
+        </a>
+        <a class="column has-text-centered" href="https://bulma.io/documentation/columns/basics/">
+          <span class="icon is-large">
+            <i class="fab fa-3x fa-css3"></i>
+          </span>
+          <p class="title is-4" style="margin-top: 0.5em;"><strong>Modern</strong></p>
+          <p class="subtitle">Built with <strong>Flexbox</strong></p>
+        </a>
+        <a class="column has-text-centered" href="https://github.com/jgthms/bulma">
+          <span class="icon is-large">
+            <i class="fab fa-3x fa-github"></i>
+          </span>
+          <p class="title is-4" style="margin-top: 0.5em;"><strong>Free</strong></p>
+          <p class="subtitle">Open source on <strong>GitHub</strong></p>
+        </a>
+      </nav>
+    </div>
+  </div>
+</section>
+-->
+
+<!--
+<section class="section is-medium">
+  <div class="container">
+    <h3 class="title is-2">
+      <span class="icon is-medium">
+        <svg class="svg-inline--fa fa-th-large fa-w-16" aria-hidden="true" data-fa-processed="" data-prefix="fas" data-icon="th-large" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M296 32h192c13.255 0 24 10.745 24 24v160c0 13.255-10.745 24-24 24H296c-13.255 0-24-10.745-24-24V56c0-13.255 10.745-24 24-24zm-80 0H24C10.745 32 0 42.745 0 56v160c0 13.255 10.745 24 24 24h192c13.255 0 24-10.745 24-24V56c0-13.255-10.745-24-24-24zM0 296v160c0 13.255 10.745 24 24 24h192c13.255 0 24-10.745 24-24V296c0-13.255-10.745-24-24-24H24c-13.255 0-24 10.745-24 24zm296 184h192c13.255 0 24-10.745 24-24V296c0-13.255-10.745-24-24-24H296c-13.255 0-24 10.745-24 24v160c0 13.255 10.745 24 24 24z"></path></svg>
+      </span>
+      <a href="https://bulma.io/documentation/layout/tiles">
+        Magic <strong>tiles</strong>
+      </a>
+    </h3>
+    <h4 class="subtitle is-4">A single element for a Metro UI-style CSS grid</h4>
+    <div class="tile is-ancestor">
+      <div class="tile is-vertical is-8">
+        <div class="tile">
+          <div class="tile is-parent is-vertical">
+            <article class="tile is-child notification is-primary">
+              <p class="title">Vertical...</p>
+              <p class="subtitle">Top tile</p>
+            </article>
+            <article class="tile is-child notification is-warning">
+              <p class="title">...tiles</p>
+              <p class="subtitle">Bottom tile</p>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child notification is-info">
+              <p class="title">Middle tile</p>
+              <p class="subtitle">With an image</p>
+              <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/640x480.png">
+              </figure>
+            </article>
+          </div>
+        </div>
+        <div class="tile is-parent">
+          <article class="tile is-child notification is-danger">
+            <p class="title">Wide tile</p>
+            <p class="subtitle">Aligned with the right tile</p>
+            <div class="content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+            </div>
+          </article>
+        </div>
+      </div>
+      <div class="tile is-parent">
+        <article class="tile is-child notification is-success">
+          <p class="title">Tall tile</p>
+          <p class="subtitle">With even more content</p>
+          <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
+            <p>Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula a sagittis. Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor lorem, et sollicitudin felis neque sit amet erat. Maecenas imperdiet felis nisi, fringilla luctus felis hendrerit sit amet. Aenean vitae gravida diam, finibus dignissim turpis. Sed eget varius ligula, at volutpat tortor.</p>
+            <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
+          </div>
+        </article>
+      </div>
+    </div>
+    <div class="highlight-full">
+<figure class="highlight"><pre><code class="language-html" data-lang="html"><span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"tile is-ancestor"</span><span class="nt">&gt;</span><br data-jekyll-commonmark-ghpages="">  <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"tile is-vertical is-8"</span><span class="nt">&gt;</span><br data-jekyll-commonmark-ghpages="">    <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"tile"</span><span class="nt">&gt;</span><br data-jekyll-commonmark-ghpages="">      <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"tile is-parent is-vertical"</span><span class="nt">&gt;</span><br data-jekyll-commonmark-ghpages="">        <span class="nt">&lt;article</span> <span class="na">class=</span><span class="s">"tile is-child"</span><span class="nt">&gt;</span><br data-jekyll-commonmark-ghpages="">          <span class="c">&lt;!-- Any content you want here --&gt;</span><br data-jekyll-commonmark-ghpages="">        <span class="nt">&lt;/article&gt;</span><br data-jekyll-commonmark-ghpages="">        <span class="nt">&lt;article</span> <span class="na">class=</span><span class="s">"tile is-child"</span><span class="nt">&gt;</span><br data-jekyll-commonmark-ghpages="">          <span class="c">&lt;!-- Any content you want here --&gt;</span><br data-jekyll-commonmark-ghpages="">        <span class="nt">&lt;/article&gt;</span><br data-jekyll-commonmark-ghpages="">      <span class="nt">&lt;/div&gt;</span><br data-jekyll-commonmark-ghpages="">      <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"tile is-parent"</span><span class="nt">&gt;</span><br data-jekyll-commonmark-ghpages="">        <span class="nt">&lt;article</span> <span class="na">class=</span><span class="s">"tile is-child"</span><span class="nt">&gt;</span><br data-jekyll-commonmark-ghpages="">          <span class="c">&lt;!-- Any content you want here --&gt;</span><br data-jekyll-commonmark-ghpages="">        <span class="nt">&lt;/article&gt;</span><br data-jekyll-commonmark-ghpages="">      <span class="nt">&lt;/div&gt;</span><br data-jekyll-commonmark-ghpages="">    <span class="nt">&lt;/div&gt;</span><br data-jekyll-commonmark-ghpages="">    <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"tile is-parent"</span><span class="nt">&gt;</span><br data-jekyll-commonmark-ghpages="">      <span class="nt">&lt;article</span> <span class="na">class=</span><span class="s">"tile is-child"</span><span class="nt">&gt;</span><br data-jekyll-commonmark-ghpages="">        <span class="c">&lt;!-- Any content you want here --&gt;</span><br data-jekyll-commonmark-ghpages="">      <span class="nt">&lt;/article&gt;</span><br data-jekyll-commonmark-ghpages="">    <span class="nt">&lt;/div&gt;</span><br data-jekyll-commonmark-ghpages="">  <span class="nt">&lt;/div&gt;</span><br data-jekyll-commonmark-ghpages="">  <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"tile is-parent"</span><span class="nt">&gt;</span><br data-jekyll-commonmark-ghpages="">    <span class="nt">&lt;article</span> <span class="na">class=</span><span class="s">"tile is-child"</span><span class="nt">&gt;</span><br data-jekyll-commonmark-ghpages="">      <span class="c">&lt;!-- Any content you want here --&gt;</span><br data-jekyll-commonmark-ghpages="">    <span class="nt">&lt;/article&gt;</span><br data-jekyll-commonmark-ghpages="">  <span class="nt">&lt;/div&gt;</span><br data-jekyll-commonmark-ghpages=""><span class="nt">&lt;/div&gt;</span></code></pre><button class="button is-small bd-copy">Copy</button></figure>
+  </div>
+    </div>
+</section>
+-->
