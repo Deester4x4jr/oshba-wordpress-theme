b64656aed227f2c58251ecaeaa3fd8adb0d1cdb4
Deester4x4jr <josh@thaw.io>
2018-02-12 12:02:47 -0700

    Theme changes committed by Gulp-Git - 2/12/2018, 12:02:47 PM

--- blocks/home/section-page-blocks.php
+++ blocks/home/section-page-blocks.php
@@ -104,7 +104,7 @@
       <div class="columns">
 
         <div class="column is-half has-text-centered">
-			<img src="/wp-content/uploads/2018/02/house-2.png" />
+			<img style="max-width:80%;" src="/wp-content/uploads/2018/02/house-2.png" />
 		</div>
 
 		<div class="column is-content is-half">
