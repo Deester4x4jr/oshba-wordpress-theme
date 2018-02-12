b728bfd6a435bcdeb46d7661eea2fc0daafee1a4
Deester4x4jr <josh@thaw.io>
2018-02-12 12:16:13 -0700

    Theme changes committed by Gulp-Git - 2/12/2018, 12:16:13 PM

--- blocks/home/section-page-blocks.php
+++ blocks/home/section-page-blocks.php
@@ -151,7 +151,7 @@
           </div>
         </div>
         <div class="tile is-parent">
-          <article class="tile is-child notification is-light">
+          <article class="tile is-child notification is-dark">
             <p class="title">Tiles can be very wide</p>
             <p class="subtitle">With a ton of text</p>
             <div class="content">
