62273017694f2bb33178c594acda0c50f2b005a7
Deester4x4jr <josh@thaw.io>
2018-02-12 10:18:23 -0700

    Theme changes committed by Gulp-Git - 2/12/2018, 10:18:23 AM

--- blocks/home/section-sponsor-bar.php
+++ blocks/home/section-sponsor-bar.php
@@ -1,5 +1,44 @@
 <?php
 
-	// ...
+	// Get items for slider
+	$posts = get_posts(array(
+	    'post_type'      => 'sponsor',
+	    'orderby'        => 'title',
+	    'order'          => 'ASC',
+	));
 
 ?>
+
+<!-- Sponsor Slider Section -->
+<section>
+  <div class="slider sponsors">
+    <?php
+      
+      foreach ($posts as $post) {
+        
+        setup_postdata($post);
+
+        echo '<a href="'.get_field('url').'"><img src="'.get_field('logo').'" /></a>';
+
+        wp_reset_postdata();
+      }
+
+    ?>
+  </div>
+</section>
+<!-- End Sponsor Slider Section -->
+
+<script type="text/javascript">
+
+  jQuery(document).ready(function($) {
+    
+    $('.sponsors').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      speed: 300,
+      pauseOnHover: true,
+    });
+
+  });
+
+</script>
