e27e540a055e85e4ba5c8dc60ffe6db88794a9bf
Deester4x4jr <josh@thaw.io>
2018-02-11 21:15:22 -0700

    Theme changes committed by Gulp-Git - 2/11/2018, 9:15:22 PM

--- header.php
+++ header.php
@@ -18,17 +18,17 @@
 		<meta property="og:url" content="http://www.thaw.io" />
 		<meta property="og:title" content="Some Cool Title">
 		<meta property="og:description" content="Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.">
-		<!-- <meta property="og:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-horizontal.png"> -->
-		<meta property="og:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-vertical.png">
-		<meta property="og:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-square.png">
+		<meta property="og:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-horizontal.png">
+		<!-- <meta property="og:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-vertical.png">
+		<meta property="og:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-square.png"> -->
 
 		<meta name="twitter:card" content="summary_large_image" />
 		<meta name="twitter:description" content="Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis." />
 		<meta name="twitter:title" content="Some Cool Title" />
 		<meta name="twitter:site" content="@Deester4x4jr" />
-		<!-- <meta name="twitter:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-vertical.png" />
-		<meta name="twitter:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-square.png" /> -->
-		<meta name="twitter:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-horizontal.png" />
+		<meta name="twitter:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-vertical.png" />
+		<meta name="twitter:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-square.png" />
+		<!-- <meta name="twitter:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-horizontal.png" /> -->
 
 		<?php wp_head(); ?>
 		<script>
