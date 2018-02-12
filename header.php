c7ffc7f3c3175d06a8119dcc035db4a4b80338c7
Deester4x4jr <josh@thaw.io>
2018-02-11 21:46:24 -0700

    Theme changes committed by Gulp-Git - 2/11/2018, 9:46:24 PM

--- header.php
+++ header.php
@@ -18,25 +18,21 @@
 		<meta itemprop="description" content="Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis." />
 		<meta itemprop="image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-horizontal.png" />
 
+		<!-- Facebook/LinkedIn Social Sharing -->
 		<meta property="og:locale" content="en_US" />
 		<meta property="og:type" content="article" />
 		<meta property="og:site_name" content="Thaw Marketing" />
 		<meta property="og:url" content="http://www.thaw.io" />
-
 		<meta property="og:title" content="Some Cool Title">
 		<meta property="og:description" content="Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.">
-
-		<!-- Facebook Sharing Image (Horizontal Orientation) -->
 		<meta property="og:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-horizontal.png">
 
-		<!-- Pinterest Sharing Options (Vertical & Orientation) -->
+		<!-- Pinterest Sharing Options (Vertical & Square Orientation) -->
 		<meta property="og:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-vertical.png">
 		<meta property="og:image" content="http://www.thaw.io/wp-content/uploads/2018/02/og-image-square.png">
 
-		<!-- Twitter Card Sharing -->
+		<!-- Twitter Card Extra Stuff -->
 		<meta name="twitter:card" content="summary_large_image" />
-		<meta name="twitter:description" content="Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis." />
-		<meta name="twitter:title" content="Some Cool Title" />
 		<meta name="twitter:site" content="@Deester4x4jr" />
 
 		<?php wp_head(); ?>
