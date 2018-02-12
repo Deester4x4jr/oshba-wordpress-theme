d800e61cf5ec252a1e12b2ebe1d534275e2b0ff6
Deester4x4jr <josh@thaw.io>
2018-02-11 17:37:18 -0700

    Theme changes committed by Gulp-Git - 2/11/2018, 5:37:18 PM

--- blocks/social/header/facebook.php
+++ blocks/social/header/facebook.php
@@ -1,5 +1,18 @@
 <?php
 
-	
+	$url = 'https://www.facebook.com/'.the_sub_field('username');
+	$icn = 'facebook-f';
+	$msg = 'Like Us';
 
 ?>
+
+<p class="control">
+    <a target="_blank" href="<?php echo $url; ?>">
+        <span class="icon">
+            <i class="fab <?php echo $icn; ?>"></i>
+        </span>
+        <span>
+            <?php echo $msg; ?>
+        </span>
+    </a>
+</p>
