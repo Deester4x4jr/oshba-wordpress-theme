6015e43893fac8fc85d6a6e7e9fafe82bf3ecf17
Deester4x4jr <josh@thaw.io>
2018-02-11 19:19:34 -0700

    Theme changes committed by Gulp-Git - 2/11/2018, 7:19:34 PM

---
+++ blocks/social/header/facebook copy.php
@@ -0,0 +1,10 @@
+<?php
+
+    // Facebook Header Social Menu Item
+
+	$url = 'https://www.facebook.com/'.get_sub_field('username');
+	$icn = 'fab fa-facebook-f';
+	$msg = 'Like';
+	$cls = 'is-'.$network;
+
+?>
