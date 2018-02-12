e4f5c4649ea03a213c1dd0cf665f2b9554c79b73
Deester4x4jr <josh@thaw.io>
2018-02-12 12:03:58 -0700

    Theme changes committed by Gulp-Git - 2/12/2018, 12:03:58 PM

--- blocks/home/section-page-blocks.php
+++ blocks/home/section-page-blocks.php
@@ -24,7 +24,7 @@
         </div>
 
         <div class="column has-text-right">
-          <div id="introVideo" class="intro-video">
+          <div style="box-shadow: 0 0 20px black;" id="introVideo" class="intro-video">
             <div class="intro-spinner"></div>
             <div class="intro-iframe">
               <iframe width="640" height="380" src="https://www.youtube.com/embed/hefs-tGUyIc?modestbranding=1&autohide=1&showinfo=0&controls=0" frameborder="0" allowfullscreen></iframe>
